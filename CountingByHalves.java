eb938028f8f357828380a65d1198960160938db5
Chacon-Miguel <54585628+Chacon-Miguel@users.noreply.github.com>
2019-12-16 05:55:08 -0600

    Create CountingByHalves.java

---
+++ CountingByHalves.java
@@ -0,0 +1,13 @@
+public class CountingByHalves {
+    public static void main(String[] args) {
+        // Display title and divider of table
+        System.out.println("x");
+        System.out.println("------");
+        
+        // Begin counting from -10 and go up to 10 by increments of 0.5
+        for (double x = -10; x <= 10; x += 0.5) {
+            System.out.println(x);
+        }
+            
+    }
+}
